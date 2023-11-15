45ba56f61d77b0edeeafb3b98af010199d1d161a
asdruaj <41650177+asdruaj@users.noreply.github.com>
2023-11-15 02:16:08 -0400

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
   baseURL: 'https://deploy-mern-api-orcin.vercel.app/api',
-  withCredentials: false
+  withCredentials: true
 })
 
 export default instance
