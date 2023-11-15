9ba3b55fa16ef8a1e52041b9f86825e464804554
Asdrubal Aldana <asdrubal.aldana12@gmail.com>
2023-11-15 01:21:49 -0400

    vercel file added

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import recipeRoutes from './routes/recipe.routes.js'
 const app = express()
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['https://deploy-mern-frontend.vercel.app'],
   credentials: true
 }))
 app.use(morgan('dev'))
