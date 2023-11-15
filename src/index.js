56c6ee4531d476019c75ba6229b9f3aeb447287e
Asdrubal Aldana <asdrubal.aldana12@gmail.com>
2023-11-15 13:35:15 -0400

    listen update

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import app from './app.js'
 import { connectDB } from './database.js'
 
 connectDB()
-app.listen(3000)
+app.listen('https://mern-recipe-app-api-jzzq.onrender.com')
 console.log('Server on port 3000')
