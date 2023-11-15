1d37be6bb3cd7dab7a17e583afb9d71dc36967c0
asdruaj <41650177+asdruaj@users.noreply.github.com>
2023-11-15 02:02:58 -0400

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://deploy-mern-api-orcin.vercel.app',
+  baseURL: 'https://deploy-mern-api-orcin.vercel.app/api',
   withCredentials: true
 })
 
