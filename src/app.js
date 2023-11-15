5178a696b0587fbf95ee2e8df3c6abc75b3b55cf
asdruaj <41650177+asdruaj@users.noreply.github.com>
2023-11-15 01:35:22 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: 'https://deploy-mern-frontend-seven.vercel.app',
+        origin: 'https://deploy-mern-frontend-seven.vercel.app/',
         credentials: true
     }
 ))
