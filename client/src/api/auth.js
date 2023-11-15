e58017465271da5c20e0875c43929e6c12f64524
asdruaj <41650177+asdruaj@users.noreply.github.com>
2023-11-14 23:42:29 -0400

    Update auth.js

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,13 +1,9 @@
 import axios from './axios.js'
 
-export const registerRequest = user => axios.post('deploy-mern-api-orcin.vercel.app
-/register', user)
+export const registerRequest = user => axios.post('/register', user)
 
-export const loginRequest = user => axios.post('deploy-mern-api-orcin.vercel.app
-/login', user)
+export const loginRequest = user => axios.post('/login', user)
 
-export const logOutRequest = () => axios.post('deploy-mern-api-orcin.vercel.app
-/logout')
+export const logOutRequest = () => axios.post('/logout')
 
-export const verifyTokenRquest = () => axios.get('deploy-mern-api-orcin.vercel.app
-/verify')
+export const verifyTokenRquest = () => axios.get('/verify')
