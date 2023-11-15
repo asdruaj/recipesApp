4145a147974ddbe911adb514c66b79ae766b3b36
Asdrubal Aldana <asdrubal.aldana12@gmail.com>
2023-11-15 13:26:10 -0400

    axiosjs update withcredentials

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://mern-recipe-app-api-jzzq.onrender.com/api'
+  baseURL: 'https://mern-recipe-app-api-jzzq.onrender.com/api',
+  withCredentials: true
 })
 
 export default instance
